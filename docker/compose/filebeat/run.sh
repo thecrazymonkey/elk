1585601af5b5c4d6887c58812ae914b2a6766503
Ivan Kunz <ivan.kunz@openet.us>
2017-06-09 19:17:05 -0400

    added conversion and filtering

--- docker/compose/filebeat/run.sh
+++ docker/compose/filebeat/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./scripts/import_dashboards -only-index -es http://elasticsearch:9200
-curl -H 'Content-Type: application/json' -XPUT 'http://elasticsearch:9200/_template/filebeat' -d@filebeat.template.json
+#./scripts/import_dashboards -only-index -es http://elasticsearch:9200
+#curl -H 'Content-Type: application/json' -XPUT 'http://elasticsearch:9200/_template/filebeat' -d@filebeat.template.json
 filebeat -e
 tail -f /dev/null
